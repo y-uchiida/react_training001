22777aca6556274ac51898fc186e8ebd4e0e4180
yugo uchiida <y.uchiida@nirneege.com>
2022-07-23 16:27:57 +0900

    html要素をreturn して画面上に表示(レンダリング)する

--- src/index.js
+++ src/index.js
@@ -4,10 +4,21 @@ import './index.css';
 
 
 /* render に渡す関数を宣言、これをコンポーネントとして扱う */
-/* null を返す(何もしない)関数の場合、画面には何も表示されない */
+/* 要素を表示する関数 */
 const App = () => {
-  return null;
-}
+  return (
+    <>
+      <h1>hello from React App!</h1>
+      <p>JSXのルール上、最上位の要素を複数return することができない</p>
+      <p>ルートになる要素がひとつだけならOKなので、&lt;&gt;&lt; /&gt; で囲む</p>
+    </>
+  );
+};
+
+/* null を返す(何もしない)関数の場合、画面には何も表示されない */
+// const App = () => {
+//   return null;
+// }
 
 /* ReactDOM.createRoot(<html要素>)
  * React v18から導入された機能で、レンダリングの並行処理ができるようになる
