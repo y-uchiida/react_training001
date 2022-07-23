36c61ef42d261bec57e4f461392d4fe5ab9706a1
yugo uchiida <y.uchiida@nirneege.com>
2022-07-23 16:23:42 +0900

    何もしないApp()をレンダリングする

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
+/* レンダリングの機能などを持ったDOMのライブラリ？ */
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+
+/* render に渡す関数を宣言、これをコンポーネントとして扱う */
+/* null を返す(何もしない)関数の場合、画面には何も表示されない */
+const App = () => {
+  return null;
+}
+
+/* ReactDOM.createRoot(<html要素>)
+ * React v18から導入された機能で、レンダリングの並行処理ができるようになる
+ * v17 までと違っているので注意(v17 までの書き方もできるらしい)
+ */
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/* React を画面に出力する
+ * root.render(<renderするもの(コンポーネント)>)
+ * Reactでは関数名を< />で囲むと、コンポーネントとして扱うことができる
+ */
+root.render(<App />);
+
