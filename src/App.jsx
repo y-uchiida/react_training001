6c0ca05659fa86ff87ad1551c15d89c9ecb12c9b
yugo uchiida <y.uchiida@nirneege.com>
2022-07-24 00:56:26 +0900

    CSSスタイル指定の基本

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,12 @@ export const App = () => {
     console.log("button clicked");
   }
 
+  /* CSSスタイル用のオブジェクト */
+  const contentStyle = {
+    color: "darkgreen",
+    backgroundColor: "lightgreen",
+  }
+
   return (
     <>
       <h1>hello from React App!</h1>
@@ -26,6 +32,20 @@ export const App = () => {
 
       <h2>ブラウザイベントの処理</h2>
       <button onClick={onClickButton}>ボタン</button>
+
+      <h2>スタイル(CSS)の指定の基本 - 1</h2>
+      <p>要素のstyle 属性に、JavaScriptのオブジェクトを渡すことで記載できる</p>
+      <p>JavaScrpit のオブジェクトを渡すために、まず {} で囲み、そこにオブジェクトを書くので、2重ブラケットになる</p>
+      <p>JavaSCript なので、ハイフンが使えない...ハイフンを含むCSSプロパティはキャメルケースに変えて記載する</p>
+      <div style={{color: "#333", backgroundColor: "#ddd", borderRadius: "8px", padding: "8px"}}>
+        style 属性を指定したdiv要素
+      </div>
+
+      <h2>スタイル(CSS)の指定の基本 - 2</h2>
+      <p>style属性に直接オブジェクトを書くだけでなく、あらかじめ宣言したオブジェクトを渡すこともできる</p>
+      <div style={contentStyle}>
+        style 属性にオブジェクトを渡したdiv要素
+      </div>
     </>
   );
 };
