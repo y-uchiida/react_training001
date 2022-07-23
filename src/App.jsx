7f6ea3b4d1ae95d23877758ed5fa5287e5eda35c
yugo uchiida <y.uchiida@nirneege.com>
2022-07-24 00:35:35 +0900

    イベントハンドラの設定

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,30 @@
 /* export をつけて宣言することで、別ファイルから読み込むことができるようになる */
 /* 要素を表示する関数 */
 export const App = () => {
+
+  /* ボタンのクリックイベント用の関数を作成  */
+  const onClickButton = () => {
+    console.log("button clicked");
+  }
+
   return (
     <>
       <h1>hello from React App!</h1>
       <p>src/App.js から読み込まれたApp() です</p>
       <p>JSXのルール上、最上位の要素を複数return することができない</p>
       <p>ルートになる要素がひとつだけならOKなので、&lt;&gt;&lt; /&gt; で囲む</p>
+
+      {
+        /* return の中では、 {} で囲むことでJavaScriptを記述することができる */
+        console.log("message from App component")
+        /* {} はひとつの式だけを扱うことができる。そのため、複数の式(というか、ステートメント)を
+         * 記載するために使うセミコロンをつけるとエラーになる
+         */
+        // console.log("this is error"); /* <- セミコロンがついているので、これはエラー */
+      }
+
+      <h2>ブラウザイベントの処理</h2>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
